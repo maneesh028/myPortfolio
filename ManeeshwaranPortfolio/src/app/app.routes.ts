d0173877c7df61af6ad65f1d17699a12c1311170
Maneeshwaran <maneeshwaran05@gmail.com>
2025-01-05 15:01:09 +0530

    import tailwind and setup assets

--- ManeeshwaranPortfolio/src/app/app.routes.ts
+++ ManeeshwaranPortfolio/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+];
